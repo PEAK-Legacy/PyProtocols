4c6cd7ef0ea5d9d978bc124b020f2af19aea691b
PJ Eby <pje@telecommunity.com>
2003-05-14 22:16:40 +0000

    Removed zope-style declaration API; if you still need it you can get it from 'protocols.zope'.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PyProtocols@8 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/protocols/api.py
+++ src/protocols/api.py
@@ -4,11 +4,6 @@ __all__ = [
     'adapt', 'declareAdapterForType', 'declareAdapterForProtocol',
     'declareAdapterForObject', 'advise', 'declareImplementation',
     'declareAdapter', 'adviseObject',
-
-    #'instancesProvide', 'instancesDoNotProvide',
-    #'protocolImplies', 'directlyProvides', 'implements', 'doesNotImplement',
-    #'implyProtocols', 'adapterForTypes', 'adapterForProtocols',
-    #'classProvides', 'moduleProvides',
 ]
 
 _marker = object()
@@ -21,6 +16,11 @@ from adapters import NO_ADAPTER_NEEDED, DOES_NOT_SUPPORT, IMPLEMENTATION_ERROR
 from advice import addClassAdvisor, getFrameInfo
 from interfaces import IOpenProtocol, IOpenProvider, IOpenImplementor
 from interfaces import Protocol, InterfaceClass
+
+
+
+
+
 
 
 
@@ -244,126 +244,3 @@ def advise(**kw):
 
     addClassAdvisor(callback)
 
-'''def instancesProvide(klass, *protocols):
-    """Declare that instances of 'klass' directly provide 'protocols'"""
-    for p in protocols:
-        declareAdapterForType(p, NO_ADAPTER_NEEDED, klass)
-
-
-def instancesDoNotProvide(klass, *protocols):
-    """Declare that instances of 'klass' do NOT provide 'protocols'"""
-    for p in protocols:
-        declareAdapterForType(p, DOES_NOT_SUPPORT, klass)
-
-
-def protocolImplies(protocol, *protocols):
-    """Declare that 'protocol' implies 'protocols' as well as its bases"""
-    for p in protocols:
-        declareAdapterForProtocol(protocol, NO_ADAPTER_NEEDED, p)
-
-
-def directlyProvides(ob, *protocols):
-    """Declare that 'ob' directly provides 'protocols'"""
-    for p in protocols:
-        declareAdapterForObject(p, NO_ADAPTER_NEEDED, ob)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# Interface and adapter declarations - implicit declarations in classes
-
-def implements(*protocols):
-    """Declare that this class' instances directly provide 'protocols'"""
-    def callback(klass):
-        instancesProvide(klass, *protocols)
-        return klass
-    addClassAdvisor(callback)
-
-def doesNotImplement(*protocols):
-    """Declare that this class' instances do not provide 'protocols'"""
-    def callback(klass):
-        instancesDoNotProvide(klass, *protocols)
-        return klass
-    addClassAdvisor(callback)
-
-
-def implyProtocols(*protocols):
-    """Declare that this protocol implies 'protocols' as well as its bases"""
-    def callback(klass):
-        protocolImplies(klass, *protocols)
-        return klass
-    addClassAdvisor(callback)
-
-
-def adapterForTypes(protocol, types):
-    """Declare that this class adapts 'types' to 'protocol'"""
-    def callback(klass):
-        for t in types: declareAdapterForType(protocol, klass, t)
-        instancesProvide(klass, protocol)
-        return klass
-    addClassAdvisor(callback)
-
-def adapterForProtocols(protocol, protocols):
-    """Declare that this class adapts 'protocols' to 'protocol'"""
-    def callback(klass):
-        for p in protocols: declareAdapterForProtocol(protocol, klass, p)
-        instancesProvide(klass, protocol)
-        return klass
-    addClassAdvisor(callback)
-
-def classProvides(*protocols):
-    """Declare that this class itself directly provides 'protocols'"""
-    def callback(klass):
-        directlyProvides(klass, *protocols)
-        return klass
-    addClassAdvisor(callback)
-
-
-def moduleProvides(*protocols):
-    """Declare that the enclosing module directly provides 'protocols'"""
-    directlyProvides(
-        modules[_getframe(1).f_globals['__name__']],
-        *protocols
-    )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
