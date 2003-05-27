4b63d4bfb3193e3b743223b5e9a45fa632cb66e1
PJ Eby <pje@telecommunity.com>
2003-05-27 23:08:12 +0000

    Removed IMPLEMENTATION_ERROR from exported API, since it's just an implementation detail.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PyProtocols@32 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/protocols/__init__.py
+++ src/protocols/__init__.py
@@ -1,7 +1,7 @@
 """Trivial Interfaces and Adaptation"""
 
 from api import *
-from adapters import NO_ADAPTER_NEEDED, DOES_NOT_SUPPORT, IMPLEMENTATION_ERROR
+from adapters import NO_ADAPTER_NEEDED, DOES_NOT_SUPPORT
 from interfaces import *
 from advice import metamethod, supermeta
 from classic import ProviderMixin
